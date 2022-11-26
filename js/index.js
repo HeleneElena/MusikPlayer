f6bfa356a19f7e63e34d3c269bd27ac5d270d507
HeleneElena <heleneebert1@gmail.com>
2022-11-26 19:40:35 +0100

    fix: audio

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 const audio = new Audio();
 
+const tracksCard = document.getElementsByClassName('track');
 const pauseBtn = document.querySelector('.player__icon_pause');
+
+console.log(tracksCard);
 
 const playMusic = url => {
     audio.src = url;  // сохрани себе путь в url
     audio.play(); // проиграй этот путь
-}
-pauseBtn.addEventListener('click', () => {
-    playMusic('audio/Adele - Someone Like You.mp3');
-});
+};
 
-
+for (let i = 0; i <= tracksCard.length; i++) {
+    tracksCard[i].addEventListener('click', playMusic);
+};
