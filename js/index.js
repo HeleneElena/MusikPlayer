17fc7cde6dfd04e15cd25e86b893782f1900b5b5
HeleneElena <heleneebert1@gmail.com>
2022-11-27 18:57:55 +0100

    tracksCard

--- js/index.js
+++ js/index.js
@@ -1,15 +1,37 @@
 const audio = new Audio();
-
 const tracksCard = document.getElementsByClassName('track');
 const pauseBtn = document.querySelector('.player__icon_pause');
+const stopBtn = document.querySelector('.player__icon_stop');
+const player = document.querySelector('.player');
 
-console.log(tracksCard);
-
-const playMusic = url => {
-    audio.src = url;  // сохрани себе путь в url
+const playMusic = e => {
+    const trackActive = e.currentTarget;
+    audio.src = trackActive.dataset.track;  // сохрани себе путь в url
     audio.play(); // проиграй этот путь
+    pauseBtn.classList.remove('player__icon_play');
+    player.classList.add('player_active');
+
+    for (let i = 0; i <= tracksCard.length; i++) {
+        tracksCard[i].classList.remove('track_active');
+    }
+
+    trackActive.classList.add('track_active');
 };
 
 for (let i = 0; i <= tracksCard.length; i++) {
     tracksCard[i].addEventListener('click', playMusic);
-};
+}
+
+pauseBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        pauseBtn.classList.remove('.player__icon_play');
+    } else {
+        audio.pause();
+        pauseBtn.classList.add('.player__icon_play');
+    }
+});
+
+stopBtn.addEventListener('click', () => {
+
+});
